73efc59c1b0eaf599d60f95f3d207c71b5c393b1
yuukihana <avoynova00@gmail.com>
2026-03-19 16:59:06 +0200

    started week 2

---
+++ 01-JS-Basics/week-2-conditional-statements/week-2-lab/00.demo.js
@@ -0,0 +1,14 @@
+function isPositive(number) {
+  let isPositive = number > 0;
+  console.log(isPositive);
+}
+
+isPositive(4);
+
+function demo(weather) {
+  if (weather == "sunny") {
+    console.log("We will go out.");
+  }
+}
+
+demo("sunny");
---
+++ 01-JS-Basics/week-2-conditional-statements/week-2-lab/01.excellent-result.js
@@ -0,0 +1,7 @@
+function excellentResult(note){
+    if (note >= 5.50){
+        console.log("Excellent!")
+    }
+}
+
+excellentResult(6);
